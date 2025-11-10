e6f4cd14649fe684f641a542d71d1ad66073e74a
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-11-10 13:21:12 -0700

    Add initial gateway service with dependencies
    Introduces the gateway service including environment configuration, authentication server, and startup scripts. Adds all required Node.js dependencies and binaries to support Express, JWT authentication, rate limiting, and related middleware.

--- vehiculos/src/routers/loadRoutes.js
+++ vehiculos/src/routers/loadRoutes.js
@@ -4,6 +4,6 @@ const loadController = require('../controllers/loadController');
 
 
 router.get('/:id', loadController.getByVehicleId);
-router.get('/', loadController.getAllLoads);
+router.get('/', loadController.getAllLoads);    
 
 module.exports = router;
