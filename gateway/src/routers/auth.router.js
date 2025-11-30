c11bb9c0345eca31139b7086be0d4bdff54fd9fb
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-11-30 03:04:08 -0700

    Update auth.router.js
    Se elimino la restriccion de rutas explicitamente ya que se definieron en la clase de utils.

--- gateway/src/routers/auth.router.js
+++ gateway/src/routers/auth.router.js
@@ -5,21 +5,6 @@ const authenticateToken = require('../middlewares/token.middleware');
 const userController = require('../controllers/users.controller');
 
 
-const SensibleRouteLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minute
-    max: 5, // Limit each IP to 5 login requests per `window`
-    message: "Too many attempts from this IP, please try again later.",
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false,  // Disable the `X-RateLimit-*` headers
-});
-
-const StrictRouteLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
-    message: "Too many requests from this IP, please try again later.",
-    standardHeaders: true, 
-    legacyHeaders: false,  
-});
 
 // public routes
 router.post('/login', SensibleRouteLimiter, userController.handleAuthentication); // Route to handle user login
