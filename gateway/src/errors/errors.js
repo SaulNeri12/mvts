a2c41e070d31d2c0085495fedb4bd15b68a49b38
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-11-30 03:01:19 -0700

    Clases de errores personalizados
    Se crearon dos clases para la definicion de errores personalizados al igual que se predefinieron algunos de esos errores personalizados.

---
+++ gateway/src/errors/errors.js
@@ -0,0 +1,33 @@
+const AppError = require('./AppError');
+
+class NotFoundError extends AppError {
+  constructor(message = 'Recurso no encontrado') {
+    super(message, 404);
+  }
+}
+
+class ValidationError extends AppError {
+  constructor(message = 'Datos inválidos') {
+    super(message, 400);
+  }
+}
+
+class UnauthorizedError extends AppError {
+  constructor(message = 'No autorizado') {
+    super(message, 401);
+  }
+}
+
+class ConflictError extends AppError {
+  constructor(message = 'Conflicto') {
+    super(message, 409);
+  }
+}
+
+module.exports = {
+  AppError,
+  NotFoundError,
+  ValidationError,
+  UnauthorizedError,
+  ConflictError
+};
