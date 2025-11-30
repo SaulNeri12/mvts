d5ac236b18f015e2f1a48ff6f999bb0b58302b40
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-11-30 16:46:36 -0700

    Controlador para sesiones
    Se cambio de nombre al controlador de usuarios a controlador de sesiones para mantener coherencia.

---
+++ gateway/src/controllers/session.controller.js
@@ -0,0 +1,59 @@
+const refreshTokenService = require('../services/refreshToken.service');
+const authService = require('../services/auth.service');
+const sessionRepository = require('../repositories/session.repossitory');
+
+/**
+ * Controller function to handle token refresh requests.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ * @param {Object} next Middlware object.
+ */
+exports.handleTokenRefresh = async (req, res, next) => 
+{
+    try {
+        const { refreshToken } = req.body;
+        const newAccessToken =  await refreshTokenService.refreshAccessToken(refreshToken);
+        res.status(200).json({ accessToken: newAccessToken });
+    } 
+    catch (error) {
+        next(error);
+    }
+}
+
+
+/**
+ * Controller function to handle logout requests.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ * @param {Object} next Middlware object.
+ */
+exports.handleLogout = async (req, res, next) => 
+{
+    const { user_id } = req.body;
+    try {
+        await sessionRepository.singleLogout(user_id);
+        res.status(200).json();
+    } catch (error) {
+        next(error);
+    }
+}
+
+/**
+ * Controller function to handle authentication requests.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ * @param {Object} next Middlware object.
+ */
+exports.handleAuthentication = async(req, res, next) => 
+{
+  try {
+    const { userId, password } = req.body;
+    const result = await authService.authenticateUser(userId, password);
+    res.status(200).json(result);
+  } 
+  catch (error) {
+    next(error);
+  }
+} 
+
+
