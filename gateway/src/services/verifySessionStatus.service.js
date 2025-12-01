7161f717d50a18e8af8c357442a0d44221f14590
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-12-01 16:11:51 -0700

    Update verifySessionStatus.service.js
    Se agrego un nuevo servicio para verificar el estatus de la sesion para antes de acceder a una pagina html.

--- gateway/src/services/verifySessionStatus.service.js
+++ gateway/src/services/verifySessionStatus.service.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const { JsonWebTokenError } = require('jsonwebtoken');
 const sessionRepository = require('../repositories/session.repository');
 
 const {
@@ -9,19 +11,25 @@ const {
     } = require('../errors/errors');
 
 /**
- * 
+ * PR
  */
-exports.verifySessionStatus = async (userId, refreshToken) => 
+exports.verifySessionStatus = async (refreshToken) => 
 {
     try{
-        const hashedRefreshToken = crypto.makeHash('sha256').update(refreshToken).digest('hex');
-        const exist = await sessionRepository.validateRefreshTokenMatch(userId, hashedRefreshToken);
+        const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        if(!payload) throw new UnauthorizedError('Token erroneo o expirado');
+        console.log(payload)
+        const hashedRefreshToken = crypto.createHash('sha256').update(refreshToken).digest('hex');
+        const exist = await sessionRepository.validateRefreshTokenMatch(payload.userId, hashedRefreshToken);
+        console.log(exist)
         if(!exist) throw new UnauthorizedError('Token erroneo o expirado');
-        return exist;
     }
     catch(error){
         if (error instanceof RepositoryError) {
             throw new InternalError('Error interno del servidor, intente más tarde');
+        }
+        if (error instanceof JsonWebTokenError) {
+            throw new UnauthorizedError('Token erroneo o expirado');
         }
         throw error;
     }
