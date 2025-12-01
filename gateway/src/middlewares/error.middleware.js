a7928002de5cfdd953f8822bbda9f5e3d9ee1f05
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-12-01 16:27:36 -0700

    Update error.middleware.js
    Agregue un tratemiento para el error de jwt

--- gateway/src/middlewares/error.middleware.js
+++ gateway/src/middlewares/error.middleware.js
@@ -1,10 +1,15 @@
 const AppError = require('../errors/app.error');
+const { JsonWebTokenError } = require('jsonwebtoken');
 
 function errorHandler(err, req, res, next) {
 
   // Error personalizado
   if (err instanceof AppError) {
     return res.status(err.statusCode).json(err.message);
+  }
+
+  if (err instanceof JsonWebTokenError) {
+    return res.status(401).json(err.message);
   }
 
   // Error normal de Javascript o cualquier otro
