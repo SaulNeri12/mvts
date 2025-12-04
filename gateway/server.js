4e58d2c3d412393a35a96e59c258b4454081465c
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-12-03 22:57:42 -0700

    Add private lights API route and client integration
    Introduces a new private router for '/api/v1/lights' and connects it to a controller. Updates the lights client to use the correct backend service and endpoint, switching from ES module to CommonJS syntax for compatibility.

--- gateway/server.js
+++ gateway/server.js
@@ -10,6 +10,7 @@ const { initSocket } = require('./src/config/socket.config');
 const errorHandler = require('./src/middlewares/error.middleware');
 const userPublicRouter = require('./src/routers/public/session.router');
 const userPrivateRouter = require('./src/routers/private/session.router');
+const lightPrivateRouter = require('./src/routers/private/lights.router')
 
 const app = express();
 const server = http.createServer(app);
@@ -73,6 +74,7 @@ function setUpPublicRoutes(){
 function setUpPrivateRoutes(){
   // private routes
   app.use('/user', userPrivateRouter);
+  app.use('/api/v1/lights', lightPrivateRouter);
 }
 
 function setUpInternallMiddlewares(){
