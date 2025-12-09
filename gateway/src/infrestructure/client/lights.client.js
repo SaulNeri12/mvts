681409dcb319ddf1c7f75367504e625aa3ce5f79
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2025-12-08 17:08:22 -0700

    Refactor lights client and expand lights router endpoints
    Removed unused manual control methods from the lights client and improved code documentation in the alerts client. Added new endpoints to the lights router for manual control and state changes, and applied a rate limiter middleware. Also cleaned up commented code in server.js.

--- gateway/src/infrestructure/client/lights.client.js
+++ gateway/src/infrestructure/client/lights.client.js
@@ -5,27 +5,8 @@ class SessionService {
     this.api = axios.create({
       baseURL: 'http://semaforos:3050',
       timeout: 3000,
-      headers: { 'Content-Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json' } 
     });
-  }
-
-  
-  async takeManualControl(userId, lightId){
-    try 
-    {
-      const response = await this.api.put('url', {
-          user_id: userId,
-          light_id: lightId
-        }
-      );
-      return response.data; 
-    } 
-    catch (error)
-    {
-      const errorMessage = error.response?.data;
-      throw new Error(errorMessage || 
-        'Error inesperado al tomar control de manual');
-    }
   }
 
   async changeLightState(lightId, newState)
@@ -43,6 +24,7 @@ class SessionService {
     }
   }
 
+
   async getAllLights(){
     try{
       const response = await this.api.get('/api/v1/semaforos');
@@ -51,20 +33,6 @@ class SessionService {
       console.log(error);
       const errorMessage = error.response?.data;
       throw new Error(errorMessage || 'Error al obtener los semaforos' );
-    }
-  }
-
-  async releaseManualControl(lightId, newState)
-  {
-    try{
-        return await this.api.patch('', { 
-            light_id: lightId,
-          }
-        );
-    }
-    catch(error){
-      const errorMessage = error.response?.data;
-      throw new Error(errorMessage || 'Error al inter liberar el control manual' );
     }
   }
 
--- gateway/src/routers/private/lights.router.js
+++ gateway/src/routers/private/lights.router.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const rateLimiter = require('../../utils/rateLimiter');
 const lightsController = require('../../controllers/lights.controller');
 
+router.use(rateLimiter.private());
 
 router.get('/', lightsController.handleGetAllLights); // Route to handle incoming lights messages
+router.post('/manual/control', lightsController.handleTakeManualControl);  
+router.patch('/change/state', lightsController.handleLightStateChange);
+router.delete('/light/manual/control', lightsController.handleReleaseManualControl);
 
 module.exports = router;
